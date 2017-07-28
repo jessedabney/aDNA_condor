69fb2c26498f4947b68cca001792d5392ad111ca
jessedabney <jesse.dabney@gmail.com>
2017-07-28 15:48:51 -0500

    fix path

--- mapdamage.sh
+++ mapdamage.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-/opt/PepPrograms/mapDamage/bin/mapdamage -i $1_merged_sort_TN.bam -r $2
-/opt/PepPrograms/mapDamage/bin/mapdamage --forward -i $1_r1_sort_TN.bam -r $2
-/opt/PepPrograms/mapDamage/bin/mapdamage --reverse -i $1_r2_sort_TN.bam -r $2
+/opt/PepPrograms/mapDamage/bin/mapDamage -i $1_merged_sort_TN.bam -r $2
+/opt/PepPrograms/mapDamage/bin/mapDamage --forward -i $1_r1_sort_TN.bam -r $2
+/opt/PepPrograms/mapDamage/bin/mapDamage --reverse -i $1_r2_sort_TN.bam -r $2
